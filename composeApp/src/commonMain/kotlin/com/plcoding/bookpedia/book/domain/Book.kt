8f1101eed733dbece3f3ba21b8b4f1f9a0b30c37
Michael Fahimi <mclfahimi@gmail.com>
2025-04-29 11:16:52 -0400

    initial setup

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/domain/Book.kt
@@ -0,0 +1,20 @@
+package com.plcoding.bookpedia.book.domain
+
+
+//data class
+// change to data layer ?
+// how to map
+
+data class Book(
+    val id: String,
+    val title: String,
+    val imageUrl: String,
+    val authors: List<String>,
+    val description: String?,
+    val languages: List<String>,
+    val firstPublishYear: String?,
+    val averageRating: Double?,
+    val ratingCount: Int?,
+    val numPages: Int?,
+    val numEditions: Int
+)
